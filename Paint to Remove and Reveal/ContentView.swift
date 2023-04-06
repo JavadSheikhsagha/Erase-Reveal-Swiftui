a04542eab27e88a0ee43def63eb50f74fdf400df
Javad <sir.hxt@gmail.com>
2023-04-06 09:13:42 +0330

    Initial Commit

---
+++ Paint to Remove and Reveal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Paint to Remove and Reveal
+//
+//  Created by enigma 1 on 4/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Paint to Remove and Reveal/Paint_to_Remove_and_RevealApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paint_to_Remove_and_RevealApp.swift
+//  Paint to Remove and Reveal
+//
+//  Created by enigma 1 on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Paint_to_Remove_and_RevealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
